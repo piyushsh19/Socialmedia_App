9b08de7cc5de582b6495a89d8f77f0719f13f2db
Piyush Sharma <Piyush.Sharma@aegraphics.tv>
2022-05-20 14:54:49 +0530

    components and routing added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
@@ -12,6 +13,16 @@ import { AddpostComponent } from './pages/addpost/addpost.component';
 import { PagenotfoundComponent } from './pages/pagenotfound/pagenotfound.component';
 import { SigninComponent } from './pages/signin/signin.component';
 import { SignupComponent } from './pages/signup/signup.component';
+import { FormsModule } from '@angular/forms';
+import { environment } from 'src/environments/environment';
+import {  AngularFireAuthModule } from '@angular/fire/compat/auth';
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFireStorageModule } from '@angular/fire/compat/storage';
+import { AngularFireDatabaseModule } from '@angular/fire/compat/database';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { ToastrModule } from 'ngx-toastr';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [
@@ -24,11 +35,23 @@ import { SignupComponent } from './pages/signup/signup.component';
     AddpostComponent,
     PagenotfoundComponent,
     SigninComponent,
-    SignupComponent
+    SignupComponent,
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+
+    FontAwesomeModule,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot(),
+
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireAuthModule,
+    AngularFireDatabaseModule,
+    AngularFireStorageModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/pages/signup/signup.component.ts
+++ src/app/pages/signup/signup.component.ts
@@ -2,12 +2,18 @@ import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
-import { finalize, from } from 'rxjs';
+// import { finalize } from 'rxjs';
 import { AuthService } from 'src/app/services/auth.service';
 
-import { AngularFireStorageModule }  from '@angular/fire/compat/storage';
+import { AngularFireStorage }  from '@angular/fire/compat/storage';
 
-import {AngularFireDatabaseModule} from '@angular/fire/compat/database';
+
+import { AngularFireDatabase} from '@angular/fire/compat/database';
+import { imageConfig } from 'src/util/config';
+
+// import { readAndCompressImage } from 'browser-image-resizer';
+
+imageConfig
 
 
 @Component({
@@ -17,9 +23,67 @@ import {AngularFireDatabaseModule} from '@angular/fire/compat/database';
 })
 export class SignupComponent implements OnInit {
 
-  constructor() { }
+  picture: string = "";
+
+  uploadPercent: number = null as any;
+
+  constructor( private auth: AuthService, private router:Router, private db: AngularFireDatabase, private toastr: ToastrService, private storage: AngularFireStorage) { }
 
   ngOnInit(): void {
   }
+
+  onSubmit(f: NgForm) {
+
+    const {email, password, username, country, bio, name} = f.form.value;
+
+    this.auth.signUp(email, password)
+    .then((res)=>{
+      console.log(res);
+      const uid = res.user;
+
+      this.db.object(`/users/${uid}`)
+      .set({
+        id: uid,
+        name: name,
+        email: email,
+        instaUsername: username,
+        bio: bio,
+        country: country,
+        picture: this.picture
+
+      })
+    })
+    .then(()=>{
+      this.router.navigateByUrl('/');
+      this.toastr.success("SignUp Success")
+    })
+    .catch((err) =>{
+      this.toastr.error("SignUp Failed")
+    })
+
+  }
+
+  // async uploadFile(event) {
+  //   const file = event.target.files[0];
+  //   let resizedImage = await readAndCompressImage(file,imageConfig)
+
+  //   const filePath = file.name // remanethe image with uuid
+  //   const fileRef = this.storage.ref(filePath) 
+
+  //   const task = this.storage.upload(filePath,resizedImage);
+
+  //   task.percentageChanges().subscribe((percentage) =>{
+  //     this.uploadPercent = percentage;
+      
+  //   })
+
+  //   task.snapshotChanges().pipe(finalize(() => {
+  //     fileRef.getDownloadURL().subscribe((url) => {
+  //       this.picture =url;
+  //       this.toastr.success("Image Uploaded Succesfully")
+  //     })
+  //   }))
+  //   .subscribe()
+  // }
 
 }
